1e0244a59ca4d89dac2407af3312a199fbfac179
jinyujeong <wlrn92@hanmail.net>
2019-06-18 15:46:17 +0900

    upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    $('#contents').pagepiling();
     $('.timer').countTo({
         speed:2000,
     });
-    $('.slider').slick({
-        dots:true,
-      });
 });
