3763974b2cdf824c8c4afc81ecfe6c866ea10cbc
jinyujeong <wlrn92@hanmail.net>
2019-06-19 17:17:22 +0900

    upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,17 +12,39 @@ $(document).ready(function() {
     $(window).scroll(function(){
         var wScroll = $(this).scrollTop();
         
-        //text-animation
+        //main-animation
         if(wScroll>=main.offset().top-$(window).height()){
             main.addClass('show');
         }
 
+        //timer-animation
         if(wScroll>=count.offset().top-$(window).height()/3){
-            count.find('.timer').countTo({
-                speed:1500,
+            $('.timer').each(function() {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                
+                $({ countNum: $this.text()}).animate({
+                  countNum: countTo
+                },
+                {
+                  duration: 1000,
+                  easing:'linear',
+                  step: function() {
+                    $this.text(Math.floor(this.countNum));
+                  },
+                  complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                  }
+              
+                });  
+                
+                
+              
             });
         }
 
+        //portfolio-animation
         if(wScroll>=cont.eq(0).offset().top-$(window).height()/3){
             cont.eq(0).addClass('show');
         }
