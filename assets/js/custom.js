d5e1dc76111e7f5856ba5371bca6a3882da4f856
jinyujeong <wlrn92@hanmail.net>
2019-08-25 02:19:56 +0900

    upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -105,24 +105,25 @@ $("#cont1 h2").on("mouseleave",function(){
   
 
 /* cont1 animation */
-var stars=300;
-  var $stars=$(".stars");
-  var r=300;
-  for(var i=0;i<stars;i++){
-    var $star=$("<div/>").addClass("star");
-    $stars.append($star);
-  }
-  $(".star").each(function(){
-    var cur=$(this);
-    var s=0.2+(Math.random()*1);
-    var curR=r+(Math.random()*300);
-    cur.css({ 
-      transformOrigin:"0 0 "+curR+"px",
-      transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
-       
+/*
+    var stars=800;
+    var $stars=$(".stars");
+    var r=800;
+    for(var i=0;i<stars;i++){
+      var $star=$("<div/>").addClass("star");
+      $stars.append($star);
+    }
+    $(".star").each(function(){
+      var cur=$(this);
+      var s=0.2+(Math.random()*1);
+      var curR=r+(Math.random()*300);
+      cur.css({ 
+        transformOrigin:"0 0 "+curR+"px",
+        transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+         
+      })
     })
-  })
-
+*/
 
     //scroll animation
     var main = $('.cont1');
